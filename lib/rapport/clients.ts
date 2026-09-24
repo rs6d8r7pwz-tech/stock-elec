14e97d05ed97636943e0f14a1d9c9f3964335658
rs6d8r7pwz-tech <rs6d8r7pwz@privaterelay.appleid.com>
2026-09-24 14:06:09 +0200

    Rapport: nouveau client CCBE EU (assainissement, 43 ouvrages)

--- lib/rapport/clients.ts
+++ lib/rapport/clients.ts
@@ -1,11 +1,12 @@
 import type { ClientDef } from './types'
 import { CCBE_AEP } from './ccbe'
+import { CCBE_EU } from './ccbe_eu'
 
 /**
  * Clients disposant d'un contrat de relevés.
  * Pour ajouter un client : créer un fichier de config (comme ccbe.ts) et l'ajouter ici.
  */
-export const CLIENTS: ClientDef[] = [CCBE_AEP]
+export const CLIENTS: ClientDef[] = [CCBE_AEP, CCBE_EU]
 
 export function getClient(id: string): ClientDef | undefined {
   return CLIENTS.find((c) => c.id === id)
